37035009eb0e5ce49059b28d776a84f86346bbc1
Ryan Andriamahery <ryan@hei.school>
2025-03-28 10:00:15 +0300

    fix: add request price filters for GET /ingredients

--- src/main/java/edu/hei/school/restaurant/endpoint/IngredientRestController.java
+++ src/main/java/edu/hei/school/restaurant/endpoint/IngredientRestController.java
@@ -3,6 +3,7 @@ package edu.hei.school.restaurant.endpoint;
 import edu.hei.school.restaurant.model.Ingredient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
@@ -11,8 +12,25 @@ import java.util.List;
 @RestController
 public class IngredientRestController {
     @GetMapping("/ingredients")
-    public ResponseEntity<List<Ingredient>> getIngredients() {
-        return ResponseEntity.ok().body(getIngredientList());
+    public ResponseEntity<List<Ingredient>> getIngredients(@RequestParam(name = "priceMinFilter", required = false) Double priceMinFilter,
+                                                           @RequestParam(name = "priceMaxFilter", required = false) Double priceMaxFilter) {
+        List<Ingredient> ingredients = getIngredientList();
+        List<Ingredient> filteredIngredients = ingredients.stream()
+                .filter(ingredient -> {
+                    if (priceMinFilter == null && priceMaxFilter == null) {
+                        return true;
+                    }
+                    Double unitPrice = ingredient.getUnitPrice();
+                    if (priceMinFilter != null && priceMaxFilter == null) {
+                        return unitPrice >= priceMinFilter;
+                    }
+                    if (priceMinFilter == null) {
+                        return unitPrice <= priceMaxFilter;
+                    }
+                    return unitPrice >= priceMinFilter && unitPrice <= priceMaxFilter;
+                })
+                .toList();
+        return ResponseEntity.ok().body(filteredIngredients);
     }
 
     private static List<Ingredient> getIngredientList() {
