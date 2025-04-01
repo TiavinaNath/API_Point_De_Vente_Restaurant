c07b01860e431119eca277dc60d0fdccc47bbe03
Ryan Andriamahery <ryan@hei.school>
2025-04-01 11:32:09 +0300

    feat: implement PUT /ingredients using database db

--- src/main/java/edu/hei/school/restaurant/endpoint/IngredientRestController.java
+++ src/main/java/edu/hei/school/restaurant/endpoint/IngredientRestController.java
@@ -1,10 +1,19 @@
 package edu.hei.school.restaurant.endpoint;
 
+import edu.hei.school.restaurant.model.Ingredient;
+import edu.hei.school.restaurant.service.IngredientService;
+import edu.hei.school.restaurant.service.exception.ServerException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class IngredientRestController {
+    private final IngredientService ingredientService;
+
     @GetMapping("/ingredients")
     public ResponseEntity<Object> getIngredients(@RequestParam(name = "priceMinFilter", required = false) Double priceMinFilter,
                                                  @RequestParam(name = "priceMaxFilter", required = false) Double priceMaxFilter) {
@@ -18,8 +27,12 @@ public class IngredientRestController {
     }
 
     @PutMapping("/ingredients")
-    public ResponseEntity<Object> updateIngredients() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public ResponseEntity<Object> updateIngredients(@RequestBody List<Ingredient> ingredients) {
+        try {
+            return ResponseEntity.ok().body(ingredientService.saveAll(ingredients));
+        } catch (ServerException e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
     }
 
     @GetMapping("/ingredients/{id}")
