7cee7627ba9589d6f28374780ecf0e9cd8d2538c
TiavinaNath <hei.tiavina.3@gmail.com>
2025-04-19 07:18:43 +0300

    feat: delete td6 on sales point

--- src/main/java/edu/hei/school/restaurant/dto/DishOrderProjection.java
+++
@@ -1,8 +0,0 @@
-package edu.hei.school.restaurant.dto;
-
-public record DishOrderProjection(
-        Long dishId,
-        String dishName,
-        double quantity,
-        double unitPrice
-) {}
